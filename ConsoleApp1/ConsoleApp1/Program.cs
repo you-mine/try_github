6863bde0d99035d65036763f3251d3a5917b05c2
sugamakaiou <sugama@occ.co.jp>
2019-05-08 10:40:44 +0900

    commit

--- ConsoleApp1/ConsoleApp1/Program.cs
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -7,6 +7,8 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("何かキーを押したら終了します");
         }
+
     }
 }
